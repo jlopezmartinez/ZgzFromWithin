cdd406ba14b9732f20fe2d395afcc7fdca617bfd
JesusLopez <jesus.lopezmar@gmail.com>
2017-07-24 22:51:04 +0200

    Created json file and convert file to String

--- app/src/main/java/es/jesuslopez/zgzfromwithin/data/local/LocalImpl.java
+++ app/src/main/java/es/jesuslopez/zgzfromwithin/data/local/LocalImpl.java
@@ -2,6 +2,10 @@ package es.jesuslopez.zgzfromwithin.data.local;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import es.jesuslopez.zgzfromwithin.data.entity.PlaceEntity;
@@ -39,8 +43,20 @@ public class LocalImpl implements LocalApi {
     }
 
     private String getDataFromLocal() {
-        //TODO//JLOPEZ//
-        //CREATE HERE JSON AND ADD TO ASSESTS FOLDER//
-        return "";
+        StringBuilder buf = new StringBuilder();
+        InputStream json;
+        try {
+            json = context.getAssets().open("LIST_PLACES.json");
+            BufferedReader in =
+                    new BufferedReader(new InputStreamReader(json, "UTF-8"));
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                buf.append(str);
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return buf.toString();
     }
 }
--- app/src/main/java/es/jesuslopez/zgzfromwithin/data/repository/PlaceRepository.java
+++ app/src/main/java/es/jesuslopez/zgzfromwithin/data/repository/PlaceRepository.java
@@ -1,15 +1,16 @@
 package es.jesuslopez.zgzfromwithin.data.repository;
 
-import android.support.annotation.NonNull;
-
 import java.util.List;
 
 import javax.inject.Inject;
 
+import es.jesuslopez.zgzfromwithin.data.entity.PlaceEntity;
+import es.jesuslopez.zgzfromwithin.data.repository.datasource.PlaceDataSourceFactory;
 import es.jesuslopez.zgzfromwithin.data.repository.datasource.mapper.Datasource;
 import es.jesuslopez.zgzfromwithin.data.repository.datasource.mapper.MapperPlaceToPlaceEntity;
 import es.jesuslopez.zgzfromwithin.domain.model.Place;
 import io.reactivex.Observable;
+import io.reactivex.functions.Function;
 
 /**
  * Created by JesusLopez on 2/7/17.
@@ -20,9 +21,19 @@ public class PlaceRepository implements Repository {
     private final Datasource datasource;
     private final MapperPlaceToPlaceEntity mapperPlaceToPlaceEntity;
 
-    @Inject public PlaceRepository()
+    @Inject
+    public PlaceRepository(PlaceDataSourceFactory placeDataSourceFactory, MapperPlaceToPlaceEntity mapper) {
+        this.datasource = placeDataSourceFactory.getDataSource();
+        this.mapperPlaceToPlaceEntity = mapper;
+    }
+
     @Override
     public Observable<List<Place>> placeList() {
-        return null;
+        return datasource.listPlaceEntity().map(new Function<List<PlaceEntity>, List<Place>>() {
+            @Override
+            public List<Place> apply(List<PlaceEntity> placeEntities) throws Exception {
+                return mapperPlaceToPlaceEntity.map(placeEntities);
+            }
+        });
     }
 }
